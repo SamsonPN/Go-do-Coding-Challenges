ce721f96c25606d664e96b1da8de604ebd85b949
Samson Nguyen <samsonn9900@gmail.com>
2025-02-01 20:34:34 -0800

    Finished step 2 for cat challenge

--- cccat/cccat.go
+++ cccat/cccat.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"os"
 )
 
+func ReadStdIn(writer io.Writer) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		writer.Write(scanner.Bytes())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("[cccat:ReadStdIn()]:", err)
+	}
+}
+
 func ReadFile(writer io.Writer, path string) {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("[cccat:ReadFile()]:", err)
 	}
 	writer.Write(data)
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("Not enough arguments to cccat")
+		log.Fatal("[cccat:Main()]: Not enough arguments to cccat")
 	}
-	ReadFile(os.Stdout, os.Args[1])
+
+	if os.Args[1] == "-" {
+		ReadStdIn(os.Stdout)
+	} else {
+		ReadFile(os.Stdout, os.Args[1])
+	}
 }
--- cccat/cccat_test.go
+++ cccat/cccat_test.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestCccat(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("reading from text file", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		wanted := "Your heart is the size of an ocean. Go find yourself in its hidden depths. - Rumi"
+		want := "Your heart is the size of an ocean. Go find yourself in its hidden depths. - Rumi"
 		ReadFile(buffer, "./snippet.txt")
 		got := buffer.String()
-
-		if wanted != got {
-			t.Errorf("got %v, wanted %v", got, wanted)
-		}
+		assertEqual(t, want, got)
 	})
+	t.Run("reading from standard input", func(t *testing.T) {
+		want := "I Don'T Believe In Failure. It Is Not Failure If You Enjoyed The Process. - Oprah Winfrey"
+
+		// simulates command <arg> | new_command <another_arg>
+		// which passes output from command as input to new_command
+		r, w, _ := os.Pipe()
+		originalStdin := os.Stdin
+
+		os.Stdin = r
+		defer func() { os.Stdin = originalStdin }()
+		fmt.Fprintln(w, want)
+		w.Close()
+
+		/*
+			 * in order for this test to pass:
+			 * 1. we must pass the text from "want" to standard input
+				created from os.Pipe()
+			 * 2. we pass a pointer of buffer to ReadStdIn
+			 * 3. and that function will write to the buffer
+			 * 4. so if ReadStdIn CANNOT read from os.Stdin,
+				then this test fails
+		*/
+		buffer := &bytes.Buffer{}
+		ReadStdIn(buffer)
+		got := buffer.String()
+		assertEqual(t, want, got)
+	})
+}
+
+func assertEqual(t testing.TB, want, got string) {
+	t.Helper()
+	if want != got {
+		t.Errorf("got: %v, wanted: %v", got, want)
+	}
 }
